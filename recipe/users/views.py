93b8b5eefb68c1857eebaa63b51901d60461e26f
vasanth-cloud <124078738+vasanth-cloud@users.noreply.github.com>
2025-01-22 15:28:34 +0530

    second

---
+++ recipe/users/views.py
@@ -0,0 +1,34 @@
+
+
+# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.urls import reverse
+
+def register(request):
+    if request.method == 'POST':
+        if "already_in" in request.POST:
+             return redirect('login')
+        form = CustomUserCreationForm(request.POST)
+       
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        return redirect(reverse('create_recipe')) 
+    return render(request, 'users/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
