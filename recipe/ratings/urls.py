97f8ee59128da360ac0bba83cdd4f22cb8aff8a7
vasanth-cloud <124078738+vasanth-cloud@users.noreply.github.com>
2025-01-23 12:47:37 +0530

    Delete recipe directory

--- recipe/ratings/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('rate/<int:pk>/', views.rate_recipe, name='rate_recipe'),
-]
--- recipe/recipes/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from .models import Recipe, Tag
-
-admin.site.register(Recipe)
-admin.site.register(Tag)
-
-
-# Register your models here.
